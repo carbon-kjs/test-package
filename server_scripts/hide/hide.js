f5fcaf1dbc11f1a20ae77c2d31c0c36eec2b7e96
BuildTools <poreba.kris@gmail.com>
2023-05-04 17:51:48 +0200

    commit

---
+++ server_scripts/hide/hide.js
@@ -0,0 +1,14 @@
+REIEvents.removeCategories((event) => {
+  const removeCategories = [
+    "create:draining",
+    "create:automatic_shapeless",
+    "create:automatic_shaped",
+    "create:automatic_packaging",
+    "create:automatic_packing",
+    "minecraft:plugins/tag",
+  ];
+
+  removeCategories.forEach((id) => {
+    event.remove(id);
+  });
+});
